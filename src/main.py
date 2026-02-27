f2155742beb582e7fe425b7f4cb097512e49d90c
Fahad <68589534+RF-Fahad-Islam@users.noreply.github.com>
2026-02-27 19:18:43 +0600

    Divide, Multiply use in main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from utils import add, subtract
+from utils import add, subtract, divide, multiply
 
 print("Fahad")
 print("Current date and time: ", datetime.now())
 print("Addition of 5 and 3: ", add(5, 3))
 print("Subtraction of 5 and 3: ", subtract(5, 3))
+print("Division of 5 and 3: ", divide(5, 3))    
+print("Multiplication of 5 and 3: ", multiply(5, 3))
