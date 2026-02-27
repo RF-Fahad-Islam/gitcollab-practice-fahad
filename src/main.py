471515738bba99e55ca0cafb6de61728c9122891
Fahad <68589534+RF-Fahad-Islam@users.noreply.github.com>
2026-02-27 19:10:42 +0600

    Basic calculator

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 from datetime import datetime
+from utils import add, subtract
+
 print("Fahad")
 print("Current date and time: ", datetime.now())
+print("Addition of 5 and 3: ", add(5, 3))
+print("Subtraction of 5 and 3: ", subtract(5, 3))
